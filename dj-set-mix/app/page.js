ecbc4629fb970f8794b6e6b2e7b8b2b22493b73f
vasuramji52 <vasupradha.ramji@gmail.com>
2024-10-05 19:26:21 -0400

    button

---
+++ dj-set-mix/app/page.js
@@ -0,0 +1,60 @@
+'use client'
+
+import React from "react";
+import { Container, Box, Typography, AppBar, Toolbar, Button, Grid} from "@mui/material";
+import styles from "./page.module.css";
+import Head from "next/head";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
+import {useRouter} from 'next/navigation';
+import {Monoton} from 'next/font/google'
+
+const monoton = Monoton({subsets: ['latin'], weight: ['400'], color: "e1a2f2"})
+
+export default function Home() {
+  return (
+    <Container maxWidth = "100vw">
+      <Head
+        style = {{
+          flexDirection:'column',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          textAlign: 'center'
+        }}
+      >
+        <title>MIXER AI.</title>
+        <meta name = "description" content = "Create flashcard from your text" />
+      </Head>
+
+      
+        <Toolbar style = {{
+          flexDirection:'row',
+          justifyContent: 'right',
+          textAlign: 'top'
+        }}>
+          <Button color="inherit" href = "/">Login</Button>
+          <Button color = "inherit" href = "/">Sign Up</Button>
+        </Toolbar>
+        <Box sx={{textAlign: 'center', my:6}}>
+          <Typography 
+            className={monoton.className} 
+            variant="h1" 
+            style={{ 
+              flexGrow: 1, 
+              color: '#e1a2f2'  // Set the color to e1a2f2
+            }}>
+            MIXER AI.
+          </Typography>
+
+          <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/creation">
+            Get Started
+          </Button>
+        </Box>
+    </Container>
+  );
+}
