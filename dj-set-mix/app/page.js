f9521b4e6829123a82214eaabeae5cda458ac7f5
archimehta <mehtaarchi100@gmail.com>
2024-10-05 15:11:08 -0400

    UI

--- dj-set-mix/app/page.js
+++ dj-set-mix/app/page.js
@@ -16,31 +16,26 @@ import {useRouter} from 'next/navigation';
 export default function Home() {
   return (
     <Container maxWidth = "100vw">
-      <Head>
-        <title>MIXERAI.</title>
+      <Head
+        style = {{
+          flexDirection:'column',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          textAlign: 'center'
+        }}
+      >
+        <title>MIXER AI.</title>
+        <meta name = "description" content = "Create flashcard from your text" />
       </Head>
 
-      <AppBar position="static">
+      <AppBar position = "static" sx = {{width: "100%"}}>
         <Toolbar>
-          <Typography variant="h6" style={{flexGrow: 1}}>
-            MIXERAI.
-          </Typography>
-          <ClerkProvider>
-            <html lang="en">
-              <body>
-                <SignedOut>
-                  <Button color="inherit" href="/sign-in">Login</Button>
-                  <Button color="inherit" href="/sign-up">Sign Up</Button>
-                </SignedOut>
-                <SignedIn>
-                  <UserButton />
-                </SignedIn>
-              </body>
-            </html>
-          </ClerkProvider>
+          <Typography variant = "h6" style = {{flexGrow: 1}}>MIXER AI.</Typography>
+          <Button color="inherit" href = "/">Login</Button>
+          <Button color = "inherit" href = "/">Sign Up</Button>
         </Toolbar>
       </AppBar>
-
+        <title>MIXER AI.</title>
     </Container>
   );
 }
