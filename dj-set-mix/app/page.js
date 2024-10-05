e69ac96b3cc0a00cc80a26dd4e7ef21c08d01042
vasuramji52 <vasupradha.ramji@gmail.com>
2024-10-05 13:02:13 -0400

    Clerk

--- dj-set-mix/app/page.js
+++ dj-set-mix/app/page.js
@@ -4,7 +4,13 @@ import React from "react";
 import { Container, Box, Typography, AppBar, Toolbar, Button, Grid} from "@mui/material";
 import styles from "./page.module.css";
 import Head from "next/head";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
 import {useRouter} from 'next/navigation';
 
 export default function Home() {
@@ -19,13 +25,19 @@ export default function Home() {
           <Typography variant="h6" style={{flexGrow: 1}}>
             MIXERAI.
           </Typography>
-          <SignedOut>
-            <Button color="inherit" href="/sign-in">Login</Button>
-            <Button color="inherit" href="/sign-up">Sign Up</Button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+          <ClerkProvider>
+            <html lang="en">
+              <body>
+                <SignedOut>
+                  <Button color="inherit" href="/sign-in">Login</Button>
+                  <Button color="inherit" href="/sign-up">Sign Up</Button>
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </body>
+            </html>
+          </ClerkProvider>
         </Toolbar>
       </AppBar>
 
